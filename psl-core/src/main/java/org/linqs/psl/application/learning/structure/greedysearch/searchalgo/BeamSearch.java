b02ca1f04ba543f069d37ad235cedba0b0de4dc1
Dhanya Sridhar <dsridhar@ucsc.edu>
2017-12-10 15:04:26 -0800

    beam search and topdown sl

--- psl-core/src/main/java/org/linqs/psl/application/learning/structure/greedysearch/searchalgo/BeamSearch.java
+++ psl-core/src/main/java/org/linqs/psl/application/learning/structure/greedysearch/searchalgo/BeamSearch.java
@@ -25,6 +25,7 @@ import com.google.common.collect.Iterables;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Set;
 import java.util.HashSet;
@@ -39,36 +40,92 @@ public class BeamSearch extends Search{
 	 * @see ConfigManager
 	 */
 	public static final String CONFIG_PREFIX = "beamsearch";
+	public static final String BEAM_SIZE_KEY = CONFIG_PREFIX + ".beamsize";
+	public static final int BEAM_SIZE_DEFAULT = 10;
+
+	protected int beamSize;
 
 	public BeamSearch(Model model, Database rvDB, Database observedDB, ConfigBundle config, Set<Formula> unitClauses, Set<Predicate> targetPredicates, Set<Predicate> observedPredicates) {
 		super(model, rvDB, observedDB, config, unitClauses, targetPredicates, observedPredicates);
+		beamSize = config.getInt(BEAM_SIZE_KEY, BEAM_SIZE_DEFAULT);
 	}
 
 	@Override
 	protected Set<WeightedRule> doSearch(double startingScore){
 
-		Set<WeightedRule> bestRules = new HashSet<WeightedRule>();
+		double previousBestGain = 0.0;
+		double bestGain = 0.0;
+		Formula bestClause = null;
+		boolean reachedStoppingCondition = false;
 
 		Set<Formula> beam = new HashSet<Formula>();
 		for (Formula c : unitClauses){
 			beam.add(c);
 		}
 
+		while(!reachedStoppingCondition){
 
-		Formula bestClause = null;
-		Set<Formula> candidateClauses = clConstr.createCandidateClauses(beam);
-		for (Formula c: candidateClauses) {
-			bestClause = c;
-		    break;
-		}	       
-		WeightedRule bestRule = new WeightedLogicalRule(bestClause, 1.0, true);
-		bestRules.add(bestRule);
+			Set<Formula> candidateClauses = clConstr.createCandidateClauses(beam);
+			Map<Formula,Double> currentClauseGains = new HashMap<Formula,Double>();
+		
+			for(Formula cc : candidateClauses){
 
+				WeightedRule candidateRule = new WeightedLogicalRule(cc);
+				Map<WeightedRule,Double> currentModelWeightsMap = this.getRuleWeights();
+
+				this.model.addRule(candidateRule);
+				this.mpll.learn();
+
+				double currentModelScore = this.wpll.scoreModel();
+				double currentGain = currentModelScore - startingScore;
+
+				if (currentGain > 0){
+					currentClauseGains.put(cc, currentGain);	
+				}
+				
+				model.removeRule(candidateRule);
+				this.resetRuleWeights(currentModelWeightsMap);
+			}
+
+			List<Formula> topClauses = getTopEntries(currentClauseGains);
+			beam = new HashSet<Formula>();
+			for (Formula f : topClauses){
+				beam.add(f);
+			}
+
+			Formula currentBeamBestClause = topClauses.get(0);
+			double currentBeamBestGain = currentClauseGains.get(currentBeamBestClause);
+
+			if (currentBeamBestGain > bestGain){
+				bestClause = currentBeamBestClause;
+				previousBestGain = bestGain;
+				bestGain = currentBeamBestGain;
+			}
+
+			if(previousBestGain == bestGain || beam.size() == 0){
+				reachedStoppingCondition = true;
+			}
+		}
+
+		Set<WeightedRule> bestRules = new HashSet<WeightedRule>();
+		bestRules.add(new WeightedLogicalRule(bestClause));
 		return bestRules;
 
-
 	}
- 
 
+	private List<Formula> getTopEntries(Map<Formula,Double> map) {
+	    List<Map.Entry<K,V>> list = new LinkedList<>(map.entrySet());
+	    Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+	        @Override
+	        public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+	            return (o1.getValue()).compareTo(o2.getValue());
+	        }
+	    });
 
+	    List<Formula> topKResult = new ArrayList<Formula>();
+	    for(int i = k; i >= 0; i--){
+	    	topKResult.add(list.get(i).getKey());
+	    }
+	    return topKresult;
+	}
 }
